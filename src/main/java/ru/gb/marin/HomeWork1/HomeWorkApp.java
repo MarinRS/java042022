9b4ab2474ae843d3dbe9a9c1c9e6af847a57c385
User <roma_marin@mail.ru>
2022-04-16 13:36:37 +0300

    HomeWork 1 add Task 1 (fin).

--- src/main/java/ru/gb/marin/HomeWork1/HomeWorkApp.java
+++ src/main/java/ru/gb/marin/HomeWork1/HomeWorkApp.java
@@ -2,11 +2,9 @@ package ru.gb.marin.HomeWork1;
 
 public class HomeWorkApp {
 
-
-    public static void Main() {
-        System.out.println("Привет мир!");
-    }
-
+    public static void main(String[] args){
+        System.out.println("Задание 1");
+}
 
 }
 
