5982887b03b886c9b2775e14d695e9b1acec801d
User <roma_marin@mail.ru>
2022-04-16 13:17:45 +0300

    HomeWork 1 (Task 1)

--- src/main/java/ru/gb/marin/HomeWork1/HomeWorkApp.java
+++ src/main/java/ru/gb/marin/HomeWork1/HomeWorkApp.java
@@ -3,10 +3,9 @@ package ru.gb.marin.HomeWork1;
 public class HomeWorkApp {
 
 
-    public static void  myPrintText() {
-        System.out.println ("Шалом православные!");
+    public static void Main() {
+        System.out.println("Привет мир!");
     }
-
 
 
 }
--- src/main/java/ru/gb/marin/Reference.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.marin;
-
-public class Reference {
-    public static void  main(String[] args) {
-        System.out.println ("Hello, world!");
-    }
-}
